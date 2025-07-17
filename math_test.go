6b170d523411aab06b569d101a8d55db986d200e
Dayvison Favacho <dfavacho@vsb.net.br>
2025-07-16 22:52:55 -0300

    ci: :rocket: Altera teste

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 
-	total := Soma(10, 10)
+	total := Soma(20, 10)
 	if total != 30 {
 		t.Errorf("Resultado da soma %d. está incorreto, Esperado: %d", total, 30)
 	}
