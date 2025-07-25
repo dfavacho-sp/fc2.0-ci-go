b794a845effe1e6d2d8773b0783cc69591a3241a
Dayvison Favacho <dfavacho@vsb.net.br>
2025-07-24 21:09:20 -0300

    Atualiza script

--- math_test.go
+++ math_test.go
@@ -3,8 +3,10 @@ package main
 import "testing"
 
 func TestSoma(t *testing.T) {
+
 	total := Soma(20, 10)
 	if total != 30 {
 		t.Errorf("Resultado da soma %d. está incorreto, Esperado: %d", total, 30)
 	}
+	
 }
